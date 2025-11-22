e3e96cdc8b32d2bc54d4b08533d97b282d59794c
ShiCheng Lu <shichenglu0708@gmail.com>
2025-11-22 13:32:19 -0500

    try random start with use state and use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import DraggableWindow from "@/components/DraggableWindow";
 import ReactPlayer from "react-player";
@@ -13,54 +13,69 @@ export default function Home() {
   const width = 9 * 30;
   const height = 16 * 30 - 1;
 
+  const [initialPos, setInitialPos] = useState<{ x: number; y: number }>();
+
+  useEffect(() => {
+    if (typeof window != "undefined") {
+      setInitialPos({
+        x: Math.random() * (window.innerWidth - width),
+        y: Math.random() * (window.innerHeight - height),
+      });
+    } else {
+      setInitialPos({ x: 150, y: 50 });
+    }
+  }, []);
+
   return (
     <div className={styles.page}>
       <h1>Is this an website?</h1>
 
-      <DraggableWindow
-        title="For the zoomers"
-        opened={open}
-        onClose={() => {
-          setOpen(false);
-        }}
-        initialX={150}
-        initialY={50}
-      >
-        <div
-          onClick={() => {
-            setPlaying(!playing);
+      {initialPos && (
+        <DraggableWindow
+          title="For the zoomers"
+          opened={open}
+          onClose={() => {
+            setOpen(false);
           }}
+          initialX={initialPos.x}
+          initialY={initialPos.y}
         >
-          {muted ? (
-            <img
-              className={styles.audio}
-              src="icons/audio-off.svg"
-              onClick={(e) => {
-                e.stopPropagation();
-                setMuted(!muted);
-              }}
-            />
-          ) : (
-            <img
-              className={styles.audio}
-              src="icons/audio.svg"
-              onClick={(e) => {
-                e.stopPropagation();
-                setMuted(!muted);
-              }}
-            />
-          )}
+          <div
+            onClick={() => {
+              setPlaying(!playing);
+            }}
+          >
+            {muted ? (
+              <img
+                className={styles.audio}
+                src="icons/audio-off.svg"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setMuted(!muted);
+                }}
+              />
+            ) : (
+              <img
+                className={styles.audio}
+                src="icons/audio.svg"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setMuted(!muted);
+                }}
+              />
+            )}
 
-          {!playing && <img className={styles.start} src="icons/play.svg" />}
-          <ReactPlayer
-            src="subway-surfers.mp4"
-            width={width}
-            height={height}
-            playing={playing}
-            muted={muted}
-          />
-        </div>
-      </DraggableWindow>
+            {!playing && <img className={styles.start} src="icons/play.svg" />}
+            <ReactPlayer
+              src="subway-surfers.mp4"
+              width={width}
+              height={height}
+              playing={playing}
+              muted={muted}
+            />
+          </div>
+        </DraggableWindow>
+      )}
     </div>
   );
 }
