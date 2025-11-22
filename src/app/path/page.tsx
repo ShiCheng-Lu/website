413262b3c896a35eed2b07f2889c81c31ba31ada
ShiCheng Lu <shichenglu0708@gmail.com>
2025-11-22 09:01:37 -0500

    is this a webpage?

---
+++ src/app/path/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', minHeight: '100vh', alignItems: 'center'}}>
+        <h1>Is this an path?</h1>
+    </div>
+  );
+}
