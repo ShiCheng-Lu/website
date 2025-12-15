a41969114407ebe7ba90c1cc0ad4ac0ce0a355a9
ShiCheng Lu <shichenglu0708@gmail.com>
2025-12-15 00:44:00 -0500

    add stun protocol to stun servers

--- src/util/peer2peer.ts
+++ src/util/peer2peer.ts
@@ -100,9 +100,9 @@ export async function joinLobby(id: string, l: LobbyData) {
     iceServers: [
       {
         urls: [
-          "stun.l.google.com:19302",
-          "stun1.l.google.com:19302",
-          "stun2.l.google.com:19302",
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
         ],
       },
     ],
